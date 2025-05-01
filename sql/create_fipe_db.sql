fefd9d5738cb5ac68cb80ce957d31c33659ca353
defendi <alexandre.defendi@mutualizo.com>
2025-04-30 22:09:22 -0300

    FIX - DB alterate and insert new columns

--- sql/create_fipe_db.sql
+++ sql/create_fipe_db.sql
@@ -1,7 +1,7 @@
 
 -- SEQUENCE: public.fipe_vehicle_manufacturer_id_seq
 
--- DROP SEQUENCE IF EXISTS public.fipe_vehicle_manufacturer_id_seq;
+-- DROP SEQUENCE IF EXISTS public.fipe_reference_month_id_seq;
 
 CREATE SEQUENCE IF NOT EXISTS public.fipe_reference_month_id_seq
     INCREMENT 1
@@ -44,8 +44,42 @@ CREATE TABLE IF NOT EXISTS public.fipe_reference_month
     id integer NOT NULL DEFAULT nextval('fipe_reference_month_id_seq'::regclass),
     name character varying COLLATE pg_catalog."default",
     code character varying COLLATE pg_catalog."default",
+    CONSTRAINT fipe_reference_month_pkey PRIMARY KEY (id)
     CONSTRAINT fipe_reference_month_code_u UNIQUE (code)
 );
+
+ALTER TABLE IF EXISTS public.fipe_reference_month
+    ADD COLUMN uid integer;
+
+ALTER TABLE IF EXISTS public.fipe_reference_month
+    ADD COLUMN create_date timestamp without time zone;
+
+ALTER TABLE IF EXISTS public.fipe_reference_month
+    ADD COLUMN write_uid integer;
+
+ALTER TABLE IF EXISTS public.fipe_reference_month
+    ADD COLUMN write_date timestamp without time zone;
+
+ALTER TABLE IF EXISTS public.fipe_reference_month
+    ADD CONSTRAINT fipe_reference_month_pkey PRIMARY KEY (id);
+
+-- Criar a tabela de fabricantes de veículos
+
+CREATE TABLE IF NOT EXISTS public.fipe_vehicle_manufacturer
+(
+    id integer NOT NULL DEFAULT nextval('fipe_vehicle_manufacturer_id_seq'::regclass),
+    name character varying COLLATE pg_catalog."default",
+    code character varying COLLATE pg_catalog."default",
+    vehicle_type integer,
+	active boolean,
+    sequence integer,
+    create_uid integer,
+    create_date timestamp without time zone,
+    write_uid integer,
+    write_date timestamp without time zone,
+    CONSTRAINT fipe_vehicle_manufacturer_pkey PRIMARY KEY (id)
+);
+
 
 -- Criar a tabela de fabricantes de veículos
 
@@ -116,3 +150,13 @@ CREATE TABLE IF NOT EXISTS public.fipe_vehicle_model_value
         ON UPDATE NO ACTION
         ON DELETE SET NULL
 );
+
+ALTER TABLE IF EXISTS public.fipe_vehicle_model_value
+    ADD COLUMN reference_month_id integer;
+
+ALTER TABLE IF EXISTS public.fipe_vehicle_model_value
+    ADD CONSTRAINT fipe_vehicle_model_value_reference_id_fkey FOREIGN KEY (reference_month_id)
+    REFERENCES public.fipe_reference_month (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
